aa988863bc4300089f6f2bbce89bd686da80f631
Ahad Iqbal <iahad2706.com>
2025-02-05 22:57:58 +0500

    Q2

---
+++ Questions/Easy/Q2/Q2.java
@@ -0,0 +1,23 @@
+package Q2;
+
+class Solution {
+    public static void main(String[] args) {
+        int [] nums = {3,2,3}; int target = 6;
+
+        int [] array=twoSum(nums, target);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+
+        }
+        return null;
+    }
+}
