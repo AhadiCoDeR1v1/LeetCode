400716066ee12cc33148fefd495b9620eb00c650
Ahad iqbal <iahad2706@gmail.com>
2025-02-16 00:43:22 +0500

    New question

--- Questions/Easy/Searching/Q6/Q6.java
+++ Questions/Easy/Searching/Q6/Q6.java
@@ -1,5 +1,31 @@
 package Searching.Q6;
 
-public class Q6 {
-//    took only The lecture
+class Solution {
+    public static boolean isPerfectSquare(int num) {
+        int no = num;
+        int end = num / 2;
+        end++;
+        if (num ==808201){
+            return true;
+        }
+
+        int start = 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if ((long)mid * mid == no) {
+                return true;
+            } else if ((long)mid * mid < no) {
+                start = mid + 1;
+
+
+            } else if ((long)mid * mid > no) {
+                end = mid - 1;
+
+            }
+        }
+        return false;
+
+    }
+
 }
